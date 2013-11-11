b42b3de39ab7c72c21793bb69d3642e6015a0a8d
Kevin Rauwolf <sweetpea-git@tentacle.net>
2013-11-10 21:40:38 -0800

    Added message backup

--- okc-dump.py
+++ okc-dump.py
@@ -25,8 +25,14 @@ from bs4 import BeautifulSoup
 
 kLoginUrl = "https://www.okcupid.com/login"
 kQuestionUrl = "http://www.okcupid.com/profile/{}/questions"
-kQtextFinder = re.compile("^qtext_(\d+)")
-kQuestionFinder = re.compile("^question_(\d+)")
+kMessageUrl = "http://www.okcupid.com/messages"
+kQtextFinder = re.compile(r"^qtext_(\d+)")
+kQuestionFinder = re.compile(r"^question_(\d+)")
+kThreadFinder = re.compile(r"threadid=(\d+)")
+kMessageFinder = re.compile(r"^message_(\d+)")
+kTimestampFinder = re.compile(r"(\d+), 'MESSAGE_FORMAT'")
+kQuestionsPerPage = 10
+kThreadsPerPage = 30
 kConfigFile = "okc-dump.ini"
 
 class Answer(object):
@@ -44,7 +50,19 @@ class Question(object):
 		self.answers = answers
 		self.explanation = explanation
 
-def parse(page):
+class Message(object):
+	def __init__(self, mid, tid, sender, recipient, timestamp, text):
+		self.mid = mid
+		self.tid = tid
+		self.sender = sender
+		self.recipient = recipient
+		self.timestamp = timestamp
+		self.text = text
+
+def message_type_to_folder(is_sent):
+	return 2 if is_sent else 1
+
+def parse_questions(page):
 	soup = BeautifulSoup(page)
 	questions = list()
 	for question in soup.find_all(id=kQuestionFinder):
@@ -52,11 +70,11 @@ def parse(page):
 		if not prompt:
 			continue
 		qid = kQtextFinder.match(prompt["id"]).group(1)
-		question_text = prompt.contents[0].strip()
+		question_text = prompt.string
 		answer_div = prompt.find_next_sibling()
 		answers = list()
 		for response in answer_div.find_all("li"):
-			answer = response.contents[0].strip()
+			answer = response.string
 			classes = response["class"]
 			mine = "mine" in classes
 			match = "match" in classes
@@ -64,13 +82,44 @@ def parse(page):
 		explanation = None
 		textarea = answer_div.find("textarea")
 		if textarea.contents:
-			explanation = textarea.contents[0].strip()
+			explanation = textarea.string
 		importance_input = question.find(id="question_{}_importance".format(qid))
 		importance = importance_input["value"]
 		public_input = question.find(id="public_{}".format(qid))
 		public = public_input["value"] == "on"
 		questions.append(Question(qid, question_text, public, importance, answers, explanation))
 	return questions
+
+def parse_threads(page):
+	soup = BeautifulSoup(page)
+	threads = list()
+	for entry in soup.find_all(href=kThreadFinder):
+		matches = kThreadFinder.search(entry["href"])
+		threads.append(matches.group(1))
+	return threads
+
+def parse_thread(page, thread_id, username):
+	soup = BeautifulSoup(page)
+	messages = list()
+	buddy_name = soup.find(attrs={ "name": "buddyname"})['value']
+	for message in soup.find_all(id=kMessageFinder):
+		mid = kMessageFinder.match(message["id"]).group(1)
+		if "from_me" in message["class"]:
+			sender = username
+			recipient = buddy_name
+		else:
+			sender = buddy_name
+			recipient = username
+		timestamp = None
+		for script in message.find_all("script"):
+			if script.contents:
+				matches = kTimestampFinder.search(script.string)
+				if matches:
+					timestamp = matches.group(1)
+		body = message.find(class_="message_body")
+		text = "\n".join([t for t in body.stripped_strings])
+		messages.append(Message(mid, thread_id, sender, recipient, timestamp, text))
+	return messages
 
 def login(cj, opener, username, password):
 	login_data = urllib.urlencode({
@@ -84,15 +133,32 @@ def get_question_count(cj, opener, username):
 	response = opener.open(kQuestionUrl.format(username))
 	soup = BeautifulSoup(response.read())
 	count = soup.find(id="q_num_total")
-	return int(count.contents[0])
+	return int(count.string)
 
 def get_question_page(cj, opener, username, low):
 	response = opener.open(kQuestionUrl.format(username) + "?low={}".format(low))
-	questions = parse(response.read())
+	questions = parse_questions(response.read())
 	return questions
 
-def to_xml(questions):
-	root = ET.Element("questions")
+def get_thread_count(cj, opener, username, is_sent):
+	response = opener.open(kMessageUrl + "?folder={}".format(message_type_to_folder(is_sent)))
+	soup = BeautifulSoup(response.read())
+	count = soup.find(class_="last")
+	return int(count.string) * kThreadsPerPage
+
+def get_thread_page(cj, opener, username, low, is_sent):
+	response = opener.open(kMessageUrl + "?low={}&folder={}".format(low, message_type_to_folder(is_sent)))
+	threads = parse_threads(response.read())
+	return threads
+
+def get_thread(cj, opener, username, thread_id):
+	response = opener.open(kMessageUrl + "?readmsg=true&threadid={}".format(thread_id))
+	messages = parse_thread(response.read(), thread_id, username)
+	return messages
+
+def to_xml(questions, messages):
+	root = ET.Element("okc-backup")
+	questions_tag = ET.SubElement(root, "questions")
 	for question in questions:
 		attributes = {
 			"id": question.qid,
@@ -100,7 +166,7 @@ def to_xml(questions):
 		}
 		if question.public:
 			attributes["public"] = "true"
-		question_tag = ET.SubElement(root, "question", attributes)
+		question_tag = ET.SubElement(questions_tag, "question", attributes)
 		prompt_tag = ET.SubElement(question_tag, "prompt")
 		prompt_tag.text = question.text
 		responses_tag = ET.SubElement(question_tag, "responses")
@@ -115,6 +181,17 @@ def to_xml(questions):
 		if question.explanation:
 			explanation_tag = ET.SubElement(question_tag, "explanation")
 			explanation_tag.text = question.explanation
+	messages_tag = ET.SubElement(root, "messages")
+	for message in messages:
+		attributes = {
+				"id": str(message.mid),
+				"thread_id": str(message.tid),
+				"timestamp": str(message.timestamp),
+				"sender": message.sender,
+				"recipient": message.recipient,
+		}
+		message_tag = ET.SubElement(messages_tag, "message", attributes)
+		message_tag.text = message.text
 	return ET.ElementTree(root)
 
 if __name__ == "__main__":
@@ -123,15 +200,48 @@ if __name__ == "__main__":
 	cj = cookielib.CookieJar()
 	opener = urllib2.build_opener(urllib2.HTTPRedirectHandler(), urllib2.HTTPHandler(), urllib2.HTTPSHandler(), urllib2.HTTPCookieProcessor(cj))
 	username = config.get("login", "username")
-	page = login(cj, opener, username, config.get("login", "password"))
-	question_count = get_question_count(cj, opener, username)
-	sys.stderr.write("Fetching {} questions\n".format(question_count))
-	sys.stderr.flush()
+	login(cj, opener, username, config.get("login", "password"))
+	# Questions
 	questions = list()
-	for low in range(1, question_count, 10):
-		questions.extend(get_question_page(cj, opener, username, low))
-		sys.stderr.write(".")
+	messages = list()
+	# TODO parameterize
+	if False:
+		question_count = get_question_count(cj, opener, username)
+		sys.stderr.write("Fetching {} questions\n".format(question_count))
 		sys.stderr.flush()
-	sys.stderr.write("\n")
-	xml = to_xml(questions)
+		for low in range(1, question_count, kQuestionsPerPage):
+			questions.extend(get_question_page(cj, opener, username, low))
+			sys.stderr.write(".")
+			sys.stderr.flush()
+		sys.stderr.write("\n")
+	# Messages
+	# TODO parameterize
+	if True:
+		threads = set()
+		# Sent
+		thread_count = get_thread_count(cj, opener, username, True)
+		sys.stderr.write("Found {} sent message threads\n".format(thread_count))
+		sys.stderr.flush()
+		for low in range(1, thread_count, kThreadsPerPage):
+			threads.update(get_thread_page(cj, opener, username, low, True))
+			sys.stderr.write(".")
+			sys.stderr.flush()
+		sys.stderr.write("\n")
+		# Received
+		thread_count = get_thread_count(cj, opener, username, False)
+		sys.stderr.write("Found {} received message threads\n".format(thread_count))
+		sys.stderr.flush()
+		for low in range(1, thread_count, kThreadsPerPage):
+			threads.update(get_thread_page(cj, opener, username, low, False))
+			sys.stderr.write(".")
+			sys.stderr.flush()
+		sys.stderr.write("\n")
+		sys.stderr.write("Fetching threads\n")
+		sys.stderr.flush()
+		for thread in threads:
+			messages.extend(get_thread(cj, opener, username, thread))
+			sys.stderr.write(".")
+			sys.stderr.flush()
+
+	xml = to_xml(questions, messages)
 	xml.write(sys.stdout, encoding="UTF-8")
